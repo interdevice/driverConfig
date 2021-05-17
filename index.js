c6d635c37290f26a5c6e81809e12bb41f02e8425
interdevice <luizh@interdevice.com.br>
2021-05-17 14:54:43 -0300

    Faz o download do arquivo, armazena na pasta padrão do projeto e com ele preenche os formulário.

--- index.js
+++ index.js
@@ -3,38 +3,52 @@ const fs = require('fs')
 const lerXLS = require('xlsx')
 const firefox = require('selenium-webdriver/firefox')
 const proxy = require('selenium-webdriver/proxy')
+const { time } = require('console')
 const options = new firefox.Options()
 
 options.setProfile('./SeleniumProfile')
-const proxyServer='	140.227.62.35:58888'
+//const proxyServer='	140.227.62.35:58888'
 
-options.setPreference("browser.download.dir", "C:\\Users\luizh\www\selenium")
+options.setPreference("browser.download.dir", "C:\\Users\\luizh\\www\\selenium\\driverConfig")
 options.setPreference("browser.download.folderList", 2)
 options.setPreference("browser.helperApps.neverAsk.saveToDisk","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
 const driver = new Builder()
     .forBrowser('firefox')
     .setFirefoxOptions(options)
-    /* .setProxy(proxy.manual({
+    /*.setProxy(proxy.manual({
         http: proxyServer,
         https: proxyServer
-    })) */
+    }))*/
     .build()
 
+async function download(){
+    await driver.get('http://rpachallenge.com/')
+    download = await driver.findElement(By.xpath("//a[@class=' col s12 m12 l12 btn waves-effect waves-light uiColorPrimary center']"))
+    download.click()
+    var css = 'col s12 m12 l12 btn waves-effect waves-light uiColorPrimary center'
+    await driver.wait(until.elementLocated({css: css}),10000)
+    
+    busca()
+    
+}
+
 async function busca() {
-    var workbook = lerXLS.readFile('challenge.xlsx');
-    var sheet_name_list = workbook.SheetNames;
+    var workbook = lerXLS.readFile('challenge.xlsx')
+    var sheet_name_list = workbook.SheetNames
     let data = []
-    var xlData = lerXLS.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+    var xlData = lerXLS.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]], {header:"A"})
+    
     xlData.forEach((res) => {
         data.push(res)
      })
     
+    
     await driver.get('http://rpachallenge.com/')
+
     start = await driver.findElement(By.xpath("//button[@class='waves-effect col s12 m12 l12 btn-large uiColorButton']"))
     await driver.wait(until.elementLocated(By.xpath("//a[@class=' col s12 m12 l12 btn waves-effect waves-light uiColorPrimary center']")),10000)
-    download = await driver.findElement(By.xpath("//a[@class=' col s12 m12 l12 btn waves-effect waves-light uiColorPrimary center']"))
-    download.click()
+    
 
     start.click()
     
@@ -48,13 +62,13 @@ async function busca() {
         email = driver.findElement(By.xpath("//input[@ng-reflect-name='labelEmail']"))
         phone = driver.findElement(By.xpath("//input[@ng-reflect-name='labelPhone']"))
         
-        firstName.sendKeys(data[i].First_Name)
-        lastName.sendKeys(data[i].Last_Name)
-        company.sendKeys(data[i].Company_Name)
-        role.sendKeys(data[i].Role_in_Company)
-        address.sendKeys(data[i].Address)
-        email.sendKeys(data[i].Email)
-        phone.sendKeys(data[i].Phone_Number)
+        firstName.sendKeys(data[i].A)
+        lastName.sendKeys(data[i].B)
+        company.sendKeys(data[i].C)
+        role.sendKeys(data[i].D)
+        address.sendKeys(data[i].E)
+        email.sendKeys(data[i].F)
+        phone.sendKeys(data[i].G)
 
         driver.findElement(By.xpath("//input[@class='btn uiColorButton']")).click()
      }
@@ -65,9 +79,6 @@ async function busca() {
      const encodedString = await driver.takeScreenshot()
      fs.writeFileSync('./result.png', encodedString, 'base64')
 
-     await driver.executeScript('window.scrollTo(0, 0)')
-
-     //await driver.quit()
 }
 
 busca()
